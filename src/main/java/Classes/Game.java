dcc2aa67edf49c67fc308342a9952ada0c1b294e
ruo <oruofei@gmail.com>
2024-04-09 13:47:07 -0500

    game class

--- src/main/java/Classes/Game.java
+++ src/main/java/Classes/Game.java
@@ -54,7 +54,48 @@ public class Game {
         scoringCards.add(new ScoringCard(1, s));
 
         //elk
+        s = new TreeMap<>();
+        s.put(1,2);
+        s.put(2,5);
+        s.put(3,9);
+        s.put(4,13);
+        scoringCards.add(new ScoringCard(2, s));
 
+        //salmon
+        s = new TreeMap<>();
+        s.put(1,2);
+        s.put(2,5);
+        s.put(3,8);
+        s.put(4,12);
+        s.put(5,16);
+        s.put(6,20);
+        s.put(7,25);
+        // 7+ is all 25
+        scoringCards.add(new ScoringCard(3, s));
+
+        //hawk
+        s = new TreeMap<>();
+        s.put(1,2);
+        s.put(2,5);
+        s.put(3,8);
+        s.put(4,11);
+        s.put(5,14);
+        s.put(6,18);
+        s.put(7,22);
+        s.put(8,26);
+        scoringCards.add(new ScoringCard(4, s));
+
+        //fox
+        s = new TreeMap<>();
+        s.put(1,2);
+        s.put(2,5);
+        s.put(3,8);
+        s.put(4,11);
+        s.put(5,14);
+        s.put(6,18);
+        s.put(7,22);
+        s.put(8,26);
+        scoringCards.add(new ScoringCard(4, s));
 
         //scoring cards instantiation
         /*
